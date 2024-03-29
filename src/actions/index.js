373e44be32e1392fd43539548f7b115a18e6e253
xanderjensen <xanderjamesjensen@icloud.com>
2024-03-29 14:05:52 -0600

    Used axios to perform get request and setup the required reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,23 @@
+import { SET_RECENT_POSTS } from './types';
+
+import axios from 'axios';
+
 export function fetchRecentPosts() {
     return function(dispatch) {
-        console.log('hello');
+        axios.get( //'https://api.dailysmarty.com/posts'
+            // Delete both of the items bellow this when dailysmarty is running again.
+            'https://xanderjensen.devcamp.space/portfolio/portfolio_items',
+            { withCredentials: true }
+        ).then(response => {
+            console.log(response.data.portfolio_items);
+            dispatch({
+                type: "SET_RECENT_POSTS",
+                // payload: response.data.posts
+                payload: response.data.portfolio_items
+            })
+        }).catch(error => {
+            console.log("actions fetchRecentPost error", error);
+        });
+
     }
 }
